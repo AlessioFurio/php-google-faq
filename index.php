4ece4974061da4c3566274a0d578334b5041c8db
Furio Alessio <alefurio@hotmail.it>
2020-12-16 15:54:55 +0100

    installazione laravel mix

--- index.php
+++ index.php
@@ -49,7 +49,8 @@ $faqs = [
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title></title>
+        <title>php-google-faq</title>
+        <link rel="stylesheet" href="dist/app.css">
     </head>
     <body>
 
