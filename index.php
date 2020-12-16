52c906e0d1a67598ae772d847375a0098ecbb250
Furio Alessio <alefurio@hotmail.it>
2020-12-16 15:36:22 +0100

    test struttura dati

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+$faqs = [
+
+    'domanda1' => [
+        [
+            'risposta1',
+            'risposta2',
+            'risposta3',
+            'risposta4',
+            'risposta5',
+        ],
+    ],
+
+    'domanda2' => [
+        [
+            'risposta1',
+            'risposta2',
+            'risposta3',
+            'risposta4',
+            'risposta5',
+        ],
+    ],
+
+    'domanda3' => [
+        [
+            'risposta1',
+            'risposta2',
+            'risposta3',
+            'risposta4',
+            'risposta5',
+        ],
+    ],
+
+    'domanda4' => [
+        [
+            'risposta1',
+            'risposta2',
+            'risposta3',
+            'risposta4',
+            'risposta5',
+        ],
+    ],
+];
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+
+        <?php
+        foreach ($faqs as $key => $faq) { ?>
+            <h1>
+                <?php echo $key; ?>
+            </h1>
+            <?php
+            foreach ($faq as $answer) {
+
+                for ($i=0; $i <count($answer) ; $i++) { ?>
+
+                    <p> <?php echo ($answer[$i]); ?> </p>
+
+                    <?php
+                }
+            }
+        }
+        ?>
+
+    </body>
+</html>
